71f238a2f0f4b7890924479474e7291e027fc52a
qasemB <mahdicmptr@gmail.com>
2023-12-22 01:12:46 +0330

    after ok passport complete

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ColorController;
+use App\Http\Controllers\DeliveryController;
+use App\Http\Controllers\DiscountController;
+use App\Http\Controllers\GuaranteeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,105 +32,103 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::group(['prefix'=>'auth'] , function(){
+Route::group(['prefix' => 'auth'], function () {
 
-    Route::post('login' , [AuthController::class, "login"]);
-    Route::post('register' , 'AuthController@register');
+    Route::post('login', [AuthController::class, "login"]);
+    Route::post('register', [AuthController::class, "register"]);
 
-    Route::group(["middleware" => 'auth:api'],function(){
-        Route::get('user', 'AuthController@getUser');
-        Route::get('logout' , 'AuthController@logout');
+    Route::group(["middleware" => 'auth:api'], function () {
+        Route::get('user', [AuthController::class, 'getUser']);
+        Route::get('logout', [AuthController::class, 'logout']);
     });
-
 });
 
-Route::group(['prefix'=>'admin'] , function(){
-//     Route::group(["middleware" => ['auth:api']],function(){
-        // Route::group(["middleware" => ['checkPermission']],function(){
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(["middleware" => ['auth:api']], function () {
+        Route::group(["middleware" => ['checkPermission']], function () {
 
             Route::get('categories', [CategoryController::class, "index"]);
-            Route::post('categories', 'CategoryController@store');
-            Route::get('categories/{id}', 'CategoryController@show');
-            Route::put('categories/{id}', 'CategoryController@update');
-            Route::delete('categories/{id}', 'CategoryController@destroy');
+            Route::post('categories', [CategoryController::class, 'store']);
+            Route::get('categories/{id}', [CategoryController::class, 'show']);
+            Route::put('categories/{id}', [CategoryController::class, 'update']);
+            Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
 
-            Route::get('categories/{categoryId}/attributes','PropertyController@index');
-            Route::post('categories/{categoryId}/attributes','PropertyController@store');
-            Route::get('categories/attributes/{id}','PropertyController@show');
-            Route::put('categories/attributes/{id}','PropertyController@update');
-            Route::delete('categories/attributes/{id}','PropertyController@destroy');
+            Route::get('categories/{categoryId}/attributes', [PropertyController::class, 'index']);
+            Route::post('categories/{categoryId}/attributes', [PropertyController::class, 'store']);
+            Route::get('categories/attributes/{id}', [PropertyController::class, 'show']);
+            Route::put('categories/attributes/{id}', [PropertyController::class, 'update']);
+            Route::delete('categories/attributes/{id}', [PropertyController::class, 'destroy']);
 
-            Route::get('products','ProductController@index');
-            Route::get('products/all_titles','ProductController@getAllTitles');
-            Route::get('products/fewer_products','ProductController@getFewerProducts');
-            Route::get('products/toggle_notification/{id}','ProductController@toggleNotification');
-            Route::post('products','ProductController@store');
-            Route::get('products/{id}','ProductController@show');
-            Route::get('products/title_is_exist/{title}','ProductController@titleIsExist');
-            Route::put('products/{id}','ProductController@update');
-            Route::delete('products/{id}','ProductController@destroy');
-            Route::post('products/{id}/add_attr','ProductController@addAttr');
-            Route::get('products/{id}/get_attr','ProductController@getProductAttrs');
-            Route::post('products/{id}/add_image','ProductController@addImage');
-            Route::delete('products/gallery/{imageId}','ProductController@deleteImage');
-            Route::get('products/gallery/set_main/{imageId}','ProductController@setMainImage');
+            Route::get('products', [ProductController::class, 'index']);
+            Route::get('products/all_titles', [ProductController::class, 'getAllTitles']);
+            Route::get('products/fewer_products', [ProductController::class, 'getFewerProducts']);
+            Route::get('products/toggle_notification/{id}', [ProductController::class, 'toggleNotification']);
+            Route::post('products', [ProductController::class, 'store']);
+            Route::get('products/{id}', [ProductController::class, 'show']);
+            Route::get('products/title_is_exist/{title}', [ProductController::class, 'titleIsExist']);
+            Route::put('products/{id}', [ProductController::class, 'update']);
+            Route::delete('products/{id}', [ProductController::class, 'destroy']);
+            Route::post('products/{id}/add_attr', [ProductController::class, 'addAttr']);
+            Route::get('products/{id}/get_attr', [ProductController::class, 'getProductAttrs']);
+            Route::post('products/{id}/add_image', [ProductController::class, 'addImage']);
+            Route::delete('products/gallery/{imageId}', [ProductController::class, 'deleteImage']);
+            Route::get('products/gallery/set_main/{imageId}', [ProductController::class, 'setMainImage']);
 
-            Route::get('colors','ColorController@index');
-            Route::post('colors','ColorController@store');
-            Route::get('colors/{id}','ColorController@show');
-            Route::put('colors/{id}','ColorController@update');
-            Route::delete('colors/{id}','ColorController@destroy');
+            Route::get('colors', [ColorController::class, 'index']);
+            Route::post('colors', [ColorController::class, 'store']);
+            Route::get('colors/{id}', [ColorController::class, 'show']);
+            Route::put('colors/{id}', [ColorController::class, 'update']);
+            Route::delete('colors/{id}', [ColorController::class, 'destroy']);
 
-            Route::get('brands','BrandController@index');
-            Route::post('brands','BrandController@store');
-            Route::get('brands/{id}','BrandController@show');
-            Route::post('brands/{id}','BrandController@update');
-            Route::delete('brands/{id}','BrandController@destroy');
+            Route::get('brands', [BrandController::class, 'index']);
+            Route::post('brands', [BrandController::class, 'store']);
+            Route::get('brands/{id}', [BrandController::class, 'show']);
+            Route::post('brands/{id}', [BrandController::class, 'update']);
+            Route::delete('brands/{id}', [BrandController::class, 'destroy']);
 
-            Route::get('guarantees','GuaranteeController@index');
-            Route::post('guarantees','GuaranteeController@store');
-            Route::get('guarantees/{id}','GuaranteeController@show');
-            Route::put('guarantees/{id}','GuaranteeController@update');
-            Route::delete('guarantees/{id}','GuaranteeController@destroy');
+            Route::get('guarantees', [GuaranteeController::class, 'index']);
+            Route::post('guarantees', [GuaranteeController::class, 'store']);
+            Route::get('guarantees/{id}', [GuaranteeController::class, 'show']);
+            Route::put('guarantees/{id}', [GuaranteeController::class, 'update']);
+            Route::delete('guarantees/{id}', [GuaranteeController::class, 'destroy']);
 
-            Route::get('discounts','DiscountController@index');
-            Route::post('discounts','DiscountController@store');
-            Route::get('discounts/{id}','DiscountController@show');
-            Route::put('discounts/{id}','DiscountController@update');
-            Route::delete('discounts/{id}','DiscountController@destroy');
+            Route::get('discounts', [DiscountController::class, 'index']);
+            Route::post('discounts', [DiscountController::class, 'store']);
+            Route::get('discounts/{id}', [DiscountController::class, 'show']);
+            Route::put('discounts/{id}', [DiscountController::class, 'update']);
+            Route::delete('discounts/{id}', [DiscountController::class, 'destroy']);
 
-            Route::get('permissions','PermissionController@index');
-            Route::get('roles','RoleController@index');
-            Route::get('roles/{id}','RoleController@show');
-            Route::post('roles','RoleController@store');
-            Route::put('roles/{id}','RoleController@update');
-            Route::put('roles/{id}/permissions','RoleController@updatePermissions');
-            Route::delete('roles/{id}','RoleController@destroy');
-            Route::get('users','UserController@index');
-            Route::get('users/{id}','UserController@show');
-            Route::post('users','UserController@store');
-            Route::put('users/{id}','UserController@update');
-            Route::delete('users/{id}','UserController@destroy');
+            Route::get('permissions', [PermissionController::class, 'index']);
+            Route::get('roles', [RoleController::class, 'index']);
+            Route::get('roles/{id}', [RoleController::class, 'show']);
+            Route::post('roles', [RoleController::class, 'store']);
+            Route::put('roles/{id}', [RoleController::class, 'update']);
+            Route::put('roles/{id}/permissions', [RoleController::class, 'updatePermissions']);
+            Route::delete('roles/{id}', [RoleController::class, 'destroy']);
+            Route::get('users', [UserController::class, 'index']);
+            Route::get('users/{id}', [UserController::class, 'show']);
+            Route::post('users', [UserController::class, 'store']);
+            Route::put('users/{id}', [UserController::class, 'update']);
+            Route::delete('users/{id}', [UserController::class, 'destroy']);
 
-            Route::get('deliveries','DeliveryController@index');
-            Route::get('deliveries/{id}','DeliveryController@show');
-            Route::post('deliveries','DeliveryController@store');
-            Route::put('deliveries/{id}','DeliveryController@update');
-            Route::delete('deliveries/{id}','DeliveryController@destroy');
+            Route::get('deliveries', [DeliveryController::class, 'index']);
+            Route::get('deliveries/{id}', [DeliveryController::class, 'show']);
+            Route::post('deliveries', [DeliveryController::class, 'store']);
+            Route::put('deliveries/{id}', [DeliveryController::class, 'update']);
+            Route::delete('deliveries/{id}', [DeliveryController::class, 'destroy']);
 
-            Route::get('carts','CartController@index');
-            Route::post('carts','CartController@store');
-            Route::get('carts/{id}','CartController@show');
-            Route::put('carts/{id}','CartController@update');
-            Route::delete('carts/{id}','CartController@destroy');
+            Route::get('carts', [CartController::class, 'index']);
+            Route::post('carts', [CartController::class, 'store']);
+            Route::get('carts/{id}', [CartController::class, 'show']);
+            Route::put('carts/{id}', [CartController::class, 'update']);
+            Route::delete('carts/{id}', [CartController::class, 'destroy']);
 
-            Route::get('orders/orders_statistics','OrderController@getOrdersStatistics');
-            Route::get('orders/this_year_orders','OrderController@getThisYearOrders');
-            Route::get('orders','OrderController@index');
-            Route::post('orders','OrderController@store');
-            Route::get('orders/{id}','OrderController@show');
-            Route::delete('orders/{id}','OrderController@destroy');
-        // });
-//     });
-
+            Route::get('orders/orders_statistics', [OrderController::class, 'getOrdersStatistics']);
+            Route::get('orders/this_year_orders', [OrderController::class, 'getThisYearOrders']);
+            Route::get('orders', [OrderController::class, 'index']);
+            Route::post('orders', [OrderController::class, 'store']);
+            Route::get('orders/{id}', [OrderController::class, 'show']);
+            Route::delete('orders/{id}', [OrderController::class, 'destroy']);
+        });
+    });
 });
